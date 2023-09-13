7fcfd1e10ad9ffa8be55da343b54cc5d95ac852d
Joven Tan <jovenntann@gmail.com>
2023-09-13 13:57:28 +0800

    chore(dynamodb-onetable): Remove unused imports and mocks

--- libs/backend/dynamodb-onetable/src/lib/dynamodb-onetable.service.spec.ts
+++ libs/backend/dynamodb-onetable/src/lib/dynamodb-onetable.service.spec.ts
@@ -1,22 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DynamoDbService } from './dynamodb-onetable.service';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+// import { DynamoDBClient } from '@aws-sdk/client-dynamodb'; // Removed this import
 import { ConfigService } from '@nestjs/config';
 import { Table } from 'dynamodb-onetable';
 
 jest.mock('@nestjs/config');
-jest.mock('@aws-sdk/client-dynamodb');
+// jest.mock('@aws-sdk/client-dynamodb'); // Removed this mock
 jest.mock('dynamodb-onetable');
 
 describe('DynamoDbService', () => {
   let service: DynamoDbService;
   let mockConfigService: jest.Mocked<ConfigService>;
-  let mockDynamoDBClient: jest.Mocked<typeof DynamoDBClient>;
   let mockTable: jest.Mocked<typeof Table>;
 
   beforeEach(async () => {
     mockConfigService = new ConfigService() as jest.Mocked<ConfigService>;
-    mockDynamoDBClient = DynamoDBClient as jest.Mocked<typeof DynamoDBClient>;
     mockTable = Table as jest.Mocked<typeof Table>;
 
     const module: TestingModule = await Test.createTestingModule({
