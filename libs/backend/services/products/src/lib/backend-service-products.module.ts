45083a07a0fbf026a1bd284f8df08ff5dc0d624a
Joven Tan <jovenntann@gmail.com>
2023-09-13 21:51:50 +0800

    feat: Add BackendServiceProductsService to BackendServiceProductsModule

--- libs/backend/services/products/src/lib/backend-service-products.module.ts
+++ libs/backend/services/products/src/lib/backend-service-products.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DynamodbOnetableModule } from '@apple/backend/dynamodb-onetable';
+import { BackendServiceProductsService } from './backend-service-products.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [DynamodbOnetableModule],
+  imports: [DynamodbOnetableModule],
+  providers: [BackendServiceProductsService],
+  exports: [BackendServiceProductsService],
 })
 export class BackendServiceProductsModule {}
