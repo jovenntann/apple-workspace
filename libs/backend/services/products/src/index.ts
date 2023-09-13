3674edd79273c745baca9dbb7a6fda23e1242b7a
Joven Tan <jovenntann@gmail.com>
2023-09-13 21:04:35 +0800

    feat(products): add read-product dto

--- libs/backend/services/products/src/index.ts
+++ libs/backend/services/products/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/backend-service-products.module';
 export * from './lib/backend-service-products.service';
+export * from './lib/dto/read-product.dto';
 export * from './lib/dto/create-product.dto';
 export * from './lib/dto/update-product.dto';
