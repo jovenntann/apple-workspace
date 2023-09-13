7398c1c5e47cfb1235c41993fe9c1d3558020fbf
Joven Tan <jovenntann@gmail.com>
2023-09-13 20:50:48 +0800

    feat(main.ts): add CORS, validation pipe, global prefix, and Swagger documentation

--- apps/api/product-management/src/main.ts
+++ apps/api/product-management/src/main.ts
@@ -1,22 +1,57 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors();
+  
+  // Use Validation Pipe globally
+  app.useGlobalPipes(new ValidationPipe());
+  
+  // Set Global Prefix
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Product Management API')
+    .setDescription('Product management')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  
+  // Create Swagger Document
+  const document = SwaggerModule.createDocument(app, config);
+  
+  // Setup Swagger Module
+  SwaggerModule.setup('docs', app, document);
+  
+  // Define Port
   const port = process.env.PORT || 3000;
+  
+  // Listen on defined port
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  
+  // Log application running message
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  
+  // Log Swagger endpoint message
+  Logger.log(`🚀 Swagger UI available at: http://localhost:${port}/${globalPrefix}/docs`);
 }
 
 bootstrap();
