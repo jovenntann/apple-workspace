cce3f75dc0c738261ce164e5ac6bbef23379283a
Joven Tan <jovenntann@gmail.com>
2023-09-28 06:28:40 +0800

    refactor: Remove unnecessary commented out code and improve code organization

--- libs/shared/contracts/src/lib/products/product.contract.ts
+++ libs/shared/contracts/src/lib/products/product.contract.ts
@@ -1,32 +1,48 @@
 import { initContract } from '@ts-rest/core';
 import { z } from 'zod';
 
-export interface Product {
-  productId: string;
-  productName: string;
-  description?: string;
-  price: number;
-  stock: number;
-  categoryId: string;
-  created: Date;
-  updated: Date;
-}
+// export interface Product {
+//   productId: string;
+//   productName: string;
+//   description?: string;
+//   price: number;
+//   stock: number;
+//   categoryId: string;
+//   created: Date;
+//   updated: Date;
+// }
 
-export interface ProductResponse {
-  data: Product[];
-  nextCursorPointer: {
-    SK: string;
-    PK: string;
-    GSI2PK: string;
-    GSI2SK: string;
-  };
-  prevCursorPointer: {
-    SK: string;
-    PK: string;
-    GSI2PK: string;
-    GSI2SK: string;
-  };
-}
+// export interface ProductResponse {
+//   data: Product[];
+//   nextCursorPointer: {
+//     SK: string;
+//     PK: string;
+//     GSI2PK: string;
+//     GSI2SK: string;
+//   };
+//   prevCursorPointer: {
+//     SK: string;
+//     PK: string;
+//     GSI2PK: string;
+//     GSI2SK: string;
+//   };
+// }
+
+/*
+* Q: can i just use ProductSchema as Interface?
+* A: No, you cannot directly use a Zod schema as a TypeScript interface. They serve different purposes and work at different stages of the code execution.
+* A TypeScript interface is a compile-time construct used by the TypeScript compiler for type checking. It doesn't exist at runtime and doesn't provide any runtime validation or transformation capabilities.
+* On the other hand, a Zod schema is a runtime construct used for data validation and transformation at runtime. It doesn't provide any compile-time type checking.
+* However, Zod provides a utility function infer that can be used to derive a TypeScript type from a Zod schema. This can be used to create a type that can be used similarly to an interface.
+
+* In this code, Product is a type derived from ProductSchema. It can be used similarly to an interface for type checking. But remember, 
+* it's not an interface, it's a type. There are some differences between interfaces and types in TypeScript, but for most use cases, they can be used interchangeably.
+*/
+
+
+export type Product = z.infer<typeof ProductSchema>;
+export type ProductResponse = z.infer<typeof ProductResponseSchema>;
+
 
 const ProductSchema = z.object({
   productId: z.string(),
@@ -88,5 +104,28 @@ export const apiProduct = c.router({
     description: 'Get products with optional limit and reverse flag',
     metadata: { roles: ['guest', 'user'] } as const,
     strictStatusCodes: true
+  },
+  
+  createProduct: {
+    method: 'POST',
+    path: '/api/products',
+    responses: {
+      201: ProductSchema,
+    },
+    body: z.object({
+      productName: z.string(),
+      description: z.string().optional(),
+      price: z.number(),
+      stock: z.number(),
+      categoryId: z.string(),
+    }),
+    summary: 'Create a new product',
+    description: 'Create a new product',
+    metadata: { roles: ['user'] } as const,
+    strictStatusCodes: true
   }
 });
+
+// EXAMPLES:
+// created: z.string().transform((val) => new Date(val)),
+// updated: z.string().transform((val) => new Date(val))
