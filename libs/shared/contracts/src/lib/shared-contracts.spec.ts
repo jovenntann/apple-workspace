4ecd81fc06f8d35baea87d2ee6d5ae260310f5b5
Joven Tan <jovenntann@gmail.com>
2023-09-29 03:46:12 +0800

    test: remove shared-contracts.spec.ts

--- libs/shared/contracts/src/lib/shared-contracts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedContracts } from './shared-contracts';
-
-describe('sharedContracts', () => {
-  it('should work', () => {
-    expect(sharedContracts()).toEqual('shared-contracts');
-  });
-});
