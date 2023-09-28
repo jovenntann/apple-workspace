7d588bb63f9513736a10cc440572c37d9b2f5557
Joven Tan <jovenntann@gmail.com>
2023-09-29 03:06:21 +0800

    feat(shared-contracts): add contract router for products

--- libs/shared/contracts/src/lib/shared-contracts.ts
+++ libs/shared/contracts/src/lib/shared-contracts.ts
@@ -1,3 +1,8 @@
-export function sharedContracts(): string {
-  return 'shared-contracts';
-}
+import { initContract } from '@ts-rest/core';
+import { apiProduct } from './products/product.contract';
+
+const c = initContract();
+
+export const contract = c.router({
+  products: apiProduct,
+});
