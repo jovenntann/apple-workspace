22f9f8442d8cabfa0cb0875f10d5166271f9afc7
Joven Tan <jovenntann@gmail.com>
2023-09-30 04:41:02 +0800

    feat(products.client): add functions to get all users and create user

--- apps/api/product-management/src/app/products/products.client.ts
+++ apps/api/product-management/src/app/products/products.client.ts
@@ -2,14 +2,20 @@ import { initClient,  } from "@ts-rest/core";
 import { contract } from '@apple/shared/contracts';
 
 const client = initClient(contract.productManagement.products, {
-    baseUrl: 'http://localhost:3001/api',
+    baseUrl: 'http://localhost:3000/api',
     baseHeaders: {}
 });
 
 const categoriesClient = initClient(contract.productManagement.categories, {
+    baseUrl: 'http://localhost:3000/api',
+    baseHeaders: {}
+});
+
+const usersClient = initClient(contract.userManagement.users, {
     baseUrl: 'http://localhost:3001/api',
     baseHeaders: {}
 });
+
 
 
 async function run() {
@@ -60,7 +66,32 @@ async function getProductById(id: string) {
     }
 }
 
+async function getAllUsers() {
+    const { body, status } = await usersClient.findAllUsers();
+
+    if (status === 200) {
+        return body;
+    }
+
+    if (status === 400) {
+        return body;
+    }
+}
+
+async function createUser(user: { username: string, email: string }) {
+    const { body, status } = await usersClient.createUser({ body: user });
+
+    if (status === 201) {
+        return body;
+    }
+}
+
+
+
 
 run()
 getAllCategories()
 getProductById('12345')
+getAllUsers()
+createUser({ username: 'test', email: 'test@example.com' })
+
